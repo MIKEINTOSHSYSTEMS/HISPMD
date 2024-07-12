14460d86b26732958e94b9dfb9a7d3e5e9100df6
MIKEINTOSHSYSTEMS <mikeintoshsys@gmail.com>
2024-07-12 19:22:59 +0000

    added fully integrated AI chatbot for data assistance

---
+++ ai/chatbot.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>MIKEINTOSH Chatbot Integration</title>
+<meta http-equiv="Content-Security-Policy" content="default-src 'self' https: http: data: blob: 'unsafe-inline' 'unsafe-eval';">
+</head>
+<body>
+  <h1>MIKEINTOSH Chatbot Integration</h1>
+  <p>Embedded DATA Exploration Chatbot:</p>
+  <iframe src="https://dup.merqconsultancy.org:8501/" width="100%" height="600" frameborder="0"></iframe>
+</body>
+</html>
