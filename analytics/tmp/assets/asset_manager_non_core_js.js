4d392b1fbfac7b4bfdef75025a9e7f8a6ac38498
MIKEINTOSHSYSTEMS <mikeintoshsys@gmail.com>
2025-03-17 15:53:45 +0000

    Update .gitignore to excl temp files and made matomo dockerized

--- analytics/tmp/assets/asset_manager_non_core_js.js
+++
@@ -1,1 +0,0 @@
-/* Matomo Javascript - cb=ef741a309cbfa381c1ea05efd0ab1724*/
--- analytics/tmp/cache/tracker/matomocache_carbrowser632-all.php
+++
@@ -1,1 +0,0 @@
-<?php return unserialize('a:2:{s:8:"lifetime";i:1742238440;s:4:"data";s:156:"sp9853i_1h10_vmm|Mac Audio Spro|(?:Tesla/(?:(?:develop|feature|terminal-das-fsd-eap)-)?[0-9.]+|QtCarBrowser)|AFTLFT962X3(?:[);/ ]|$)|AFTLBT962E2(?:[);/ ]|$)";}', ['allowed_classes' => false]);
